99a7ee047cfd4f00c15596422cbabf5995c59711
cwkreid19141 <student+19141@codingwithkids.com>
2018-08-16 16:17:53 -0700

    Virus and Doctor simuator. 3rd coding. with virus and doctor on screen. it works with a few bugs.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ size = (width, height) = (850, 480)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
-color = (255, 205, 255)
+color = (150, 2, 100)
 bacteria = pygame.sprite.Group()
 doctors = pygame.sprite.Group()
 
@@ -17,6 +17,8 @@ bac_num = 5
 doc_num = 1
 split_time = 500
 done = False
+split_time = 5
+font = pygame.font.SysFont(None, 75)
 
 
 def process_events():
@@ -26,19 +28,36 @@ def process_events():
             done = True
             sys.exit()
 
+def init():
+    for i in range(bac_num):
+        bacteria.add(VirusNode((random.randint(50, width - 50), random.randint(50, height - 50)), split_time))
+    for i in range(doc_num):
+        doctors.add(Doctor((random.randint(50, width - 50), random.randint(50, height - 50))))
+
 
 def main():
-
-    bacteria.add(VirusNode((random.randint(50, width - 50), random.randint(50, height - 50)), split_time))
-    doctors.add(Doctor((random.randint(50, width - 50), random.randint(50, height - 50))))
+    init()
     while not done:
         clock.tick(60)
         process_events()
+        if len(bacteria) > bac_num * split_time:
+            text = font.render("You were overrunned by the virus", True, (250, 200, 250))
+            text_rect = text.get_rect()
+        elif len(bacteria) == 0:
+            text = font.render("Outbreak over", True, (250, 200, 250))
+            text_rect = text.get_rect()
+        else:
+            doctors.update()
+            bacteria.update()
+            pygame.sprite.groupcollide(doctors, bacteria, False, True)
+            text = font.render("Bacteria Count: {}".format(len(bacteria)), True, (250, 200, 250))
+            text_rect = text.get_rect()
         doctors.update()
         bacteria.update()
         screen.fill(color)
         doctors.draw(screen)
         bacteria.draw(screen)
+        screen.blit(text, text_rect)
         pygame.display.flip()
 
 
